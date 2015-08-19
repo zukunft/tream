5954572dbfb978309ce41a867c78446bb2ff98b2
Timon Zielonka <timon@zukunft.com>
2015-08-20 00:16:13 +0200

    Create actions.php

---
+++ p4a/applications/tream/objects/actions.php
@@ -0,0 +1,114 @@
+<?php
+/* 
+
+This file is part of TREAM - Portfolio Management Software.
+
+TREAM is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as
+published by the Free Software Foundation, either version 3 of
+the License, or (at your option) any later version.
+
+TREAM is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with TREAM. If not, see <http://www.gnu.org/licenses/gpl.html>.
+
+To contact the authors write to: 
+Timon Zielonka <timon@zukunft.com>
+
+Copyright (c) 2013-2015 zukunft.com AG, Zurich
+Heang Lor <heang@zukunft.com>
+
+http://tream.biz
+
+ * This file is based on P4A - PHP For Applications.
+ *
+ * To contact the authors write to:                                     
+ * Fabrizio Balliano <fabrizio@fabrizioballiano.it>                    
+ * Andrea Giardina <andrea.giardina@crealabs.it>
+ *
+ * https://github.com/fballiano/p4a
+ *
+ * @author Timon Zielonka <timon@zukunft.com>
+ * @copyright Copyright (c) 2013-2015 zukunft.com AG, Zurich
+
+*/
+
+class Actions extends P4A_Base_Mask
+{
+	public $toolbar = null;
+	public $table = null;
+	public $fs_details = null;
+	
+	public function __construct()
+	{
+		parent::__construct();
+		$p4a = p4a::singleton();
+
+		$this->setSource($p4a->actions);
+		$this->setTitle("Single activities - if a client contact leads to several planned activities, insert each activity here");
+		$this->firstRow();
+
+		// Customizing fields properties
+		$this->fields->details
+			->setType("rich_textarea")
+			->setWidth(500)
+			->setHeight(200);
+
+		$this->fields->create_contact_id
+			->setLabel("Client activity")
+			->setType("select")
+			->setSource(P4A::singleton()->select_contacts)
+			->setSourceDescriptionField("contact_select");
+
+		$this->fields->action_status_id
+			->setLabel("Status")
+			->setType("select")
+			->setSource(P4A::singleton()->select_action_stati)
+			->setSourceDescriptionField("status_text");
+
+		$this->fields->security_id
+			->setLabel("Related Security")
+			->setType("select")
+			->setSource(P4A::singleton()->select_securities)
+			->setSourceDescriptionField("select_name");
+
+		$this->fields->description->setWidth(500);
+		$this->fields->comment->setWidth(500);
+
+		$this->fields->creation_time
+			->setLabel("Created");
+
+		$this->build("p4a_full_toolbar", "toolbar")
+			->setMask($this);
+
+		$this->build("p4a_table", "table")
+			->setSource($p4a->actions)
+			->setVisibleCols(array("contact","creation_time","description","deadline","status"))
+			->setWidth(1200)
+			->showNavigationBar();
+
+		$this->build("p4a_fieldset", "fs_details")
+			->setLabel("Action detail")
+			->anchor($this->fields->create_contact_id)
+			->anchor($this->fields->creation_time)
+			->anchor($this->fields->description)
+			->anchor($this->fields->details)
+			->anchor($this->fields->security_id)
+			->anchor($this->fields->deadline)
+			->anchor($this->fields->action_status_id)
+			->anchor($this->fields->comment);
+		
+		$this->frame
+			->anchor($this->table)
+ 			->anchor($this->fs_details);
+
+		$this
+			->display("menu", $p4a->menu)
+			->display("top", $this->toolbar)
+			->setFocus($this->fields->description);
+	}
+}
