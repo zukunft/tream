bca9ed232a5cef6011f03185aa6b511572b081ff
Timon Zielonka <timon@zukunft.com>
2015-08-23 15:33:13 +0200

    Create links.php

---
+++ p4a/applications/tream/objects/links.php
@@ -0,0 +1,86 @@
+<?php
+/* 
+
+This file is part of TREAM - Portfolio Management Software.
+
+TREAM is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as
+published by the Free Software Foundation, either version 3 of
+the License, or (at your option) any later version.
+
+TREAM is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with TREAM. If not, see <http://www.gnu.org/licenses/gpl.html>.
+
+To contact the authors write to: 
+Timon Zielonka <timon@zukunft.com>
+
+Copyright (c) 2013-2015 zukunft.com AG, Zurich
+Heang Lor <heang@zukunft.com>
+
+http://tream.biz
+
+ * This file is based on P4A - PHP For Applications.
+ *
+ * To contact the authors write to:                                     
+ * Fabrizio Balliano <fabrizio@fabrizioballiano.it>                    
+ * Andrea Giardina <andrea.giardina@crealabs.it>
+ *
+ * https://github.com/fballiano/p4a
+ *
+ * @author Timon Zielonka <timon@zukunft.com>
+ * @copyright Copyright (c) 2013-2015 zukunft.com AG, Zurich
+
+*/
+class Links extends P4A_Base_Mask
+{
+	public $toolbar = null;
+	public $table = null;
+	public $fs_details = null;
+	
+	public function __construct()
+	{
+		parent::__construct();
+		$p4a = p4a::singleton();
+
+		$this->setSource($p4a->accounts);
+		$this->firstRow();
+
+		// Customizing fields properties
+		$this->fields->person_id
+			->setLabel("Person")
+			->setType("select")
+			->setSource(P4A::singleton()->select_persons)
+			->setSourceDescriptionField("select_name");
+
+		$this->build("p4a_full_toolbar", "toolbar")
+			->setMask($this);
+
+		/* usually a record does not need to be deleted */
+		$this->toolbar->buttons->delete->disable();
+
+		$this->build("p4a_table", "table")
+			->setSource($p4a->accounts)
+			->setWidth(500)
+			->setVisibleCols(array("account_name","client"))
+			->showNavigationBar();
+
+		$this->build("p4a_fieldset", "fs_details")
+			->setLabel("Account detail")
+			->anchor($this->fields->account_name)
+			->anchor($this->fields->person_id);
+		
+		$this->frame
+			->anchor($this->table)
+ 			->anchor($this->fs_details);
+
+		$this
+			->display("menu", $p4a->menu)
+			->display("top", $this->toolbar)
+			->setFocus($this->fields->person_id);
+	}
+}
