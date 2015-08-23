4f5bf7ae7df37b613f6a6823b77dc95715ad4e2b
Timon Zielonka <timon@zukunft.com>
2015-08-23 12:44:28 +0200

    Create event_types.php

---
+++ p4a/applications/tream/objects/event_types.php
@@ -0,0 +1,80 @@
+<?php
+/* 
+
+This file is part of TREAM - Portfolio Management Software.
+
+TREAM is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as
+published by the Free Software Foundation, either version 3 of
+the License, or (at your option) any later version.
+
+TREAM is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with TREAM. If not, see <http://www.gnu.org/licenses/gpl.html>.
+
+To contact the authors write to: 
+Timon Zielonka <timon@zukunft.com>
+
+Copyright (c) 2013-2015 zukunft.com AG, Zurich
+Heang Lor <heang@zukunft.com>
+
+http://tream.biz
+
+ * This file is based on P4A - PHP For Applications.
+ *
+ * To contact the authors write to:                                     
+ * Fabrizio Balliano <fabrizio@fabrizioballiano.it>                    
+ * Andrea Giardina <andrea.giardina@crealabs.it>
+ *
+ * https://github.com/fballiano/p4a
+ *
+ * @author Timon Zielonka <timon@zukunft.com>
+ * @copyright Copyright (c) 2013-2015 zukunft.com AG, Zurich
+
+*/
+class Event_types extends P4A_Base_Mask
+{
+	public $toolbar = null;
+	public $table = null;
+	public $fs_details = null;
+	
+	public function __construct()
+	{
+		parent::__construct();
+		$p4a = p4a::singleton();
+
+		$this->setSource($p4a->event_types);
+		$this->firstRow();
+
+		$this->build("p4a_full_toolbar", "toolbar")
+			->setMask($this);
+
+		/* usually a record does not need to be deleted */
+		$this->toolbar->buttons->delete->disable();
+
+		$this->build("p4a_table", "table")
+			->setSource($p4a->event_types)
+			->setWidth(500)
+			->showNavigationBar();
+
+		$this->setRequiredField("type_name");
+
+		$this->build("p4a_fieldset", "fs_details")
+			->setLabel("Event type detail")
+			->anchor($this->fields->type_name)
+			->anchor($this->fields->factor);
+		
+		$this->frame
+			->anchor($this->table)
+ 			->anchor($this->fs_details);
+
+		$this
+			->display("menu", $p4a->menu)
+			->display("top", $this->toolbar)
+			->setFocus($this->fields->type_name);
+	}
+}
